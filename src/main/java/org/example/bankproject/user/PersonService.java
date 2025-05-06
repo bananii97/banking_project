ab361b9992f74168a0878777f934d34c61a6f14c
Krystian <krystianbanach54@gmail.com>
2025-05-06 20:43:11 +0200

    User implementation

---
+++ src/main/java/org/example/bankproject/user/PersonService.java
@@ -0,0 +1,19 @@
+package org.example.bankproject.user;
+
+import lombok.RequiredArgsConstructor;
+import org.example.bankproject.user.api.PersonDto;
+import org.example.bankproject.user.jpa.Person;
+import org.example.bankproject.user.jpa.PersonRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    public Person createPerson(PersonDto personDto){
+        Person person = PersonMapper.fromDto(personDto);
+        return personRepository.save(person);
+    }
+}
---
+++ src/main/java/org/example/bankproject/user/jpa/Person.java
@@ -0,0 +1,30 @@
+package org.example.bankproject.user.jpa;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Entity
+@Table(name = "person")
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastName;
+    private String email;
+    private String nationalIdentityNumber;
+    private LocalDate birthDate;
+    private int gender;
+    private int phoneNumber;
+}
