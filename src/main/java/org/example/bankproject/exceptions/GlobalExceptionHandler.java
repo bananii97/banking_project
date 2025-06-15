6afe3098b8cc8917a95b43e8f91e841f5d8bd4f6
Krystian <krystianbanach54@gmail.com>
2025-06-15 18:20:22 +0200

    global exception handler, exceptio dto and list validation error

---
+++ src/main/java/org/example/bankproject/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package org.example.bankproject.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionDto handleException(RuntimeException exception){
+        return new ExceptionDto(exception.getMessage());
+    }
+}
