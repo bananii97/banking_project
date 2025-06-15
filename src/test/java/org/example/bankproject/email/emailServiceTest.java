3a00ef5b5a6c514efaec76bde6cb0c5f252d075b
Krystian <krystianbanach54@gmail.com>
2025-06-15 17:57:58 +0200

    Email sender and sevice

--- src/test/java/org/example/bankproject/email/emailServiceTest.java
+++ src/test/java/org/example/bankproject/email/emailServiceTest.java
@@ -2,27 +2,19 @@ package org.example.bankproject.email;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class emailServiceTest {
 
     @Mock
-    private JavaMailSender mailSender;
-
-    @InjectMocks
     private EmailService emailService;
 
     @Test
-    void emailServiceTestShouldSendEmail() {
+    void shouldSendEmailWhenValidParameters() {
         // given
         String to = "test@example.com";
         String subject = "Test Subject";
@@ -32,12 +24,6 @@ public class emailServiceTest {
         emailService.sendEmail(to, subject, text);
 
         // then
-        ArgumentCaptor<SimpleMailMessage> messageCaptor = ArgumentCaptor.forClass(SimpleMailMessage.class);
-        verify(mailSender).send(messageCaptor.capture());
-
-        SimpleMailMessage sentMessage = messageCaptor.getValue();
-        assertEquals(to, sentMessage.getTo()[0]);
-        assertEquals(subject, sentMessage.getSubject());
-        assertEquals(text, sentMessage.getText());
+        verify(emailService).sendEmail(to, subject, text);
     }
 }
