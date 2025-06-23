6aac4ae4ee83dc2cb282df1fdc88644ab283d0ce
Krystian <krystianbanach54@gmail.com>
2025-06-23 18:33:13 +0200

    exchange api

--- src/main/java/org/example/bankproject/exchange/model/ExchangeRate.java
+++ src/main/java/org/example/bankproject/exchange/model/ExchangeRate.java
@@ -1,12 +1,4 @@
 package org.example.bankproject.exchange.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class ExchangeRate {
-
-    private double buy;
-    private double sell;
+public record ExchangeRate(double buy, double sell) {
 }
