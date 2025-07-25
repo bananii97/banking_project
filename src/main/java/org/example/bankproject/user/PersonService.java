76b5f16fd863aa9312d5c297059645b5ce2a7e5b
Krystian <krystianbanach54@gmail.com>
2025-07-25 19:42:44 +0200

    code fixes

--- src/main/java/org/example/bankproject/user/PersonService.java
+++ src/main/java/org/example/bankproject/user/PersonService.java
@@ -9,6 +9,8 @@ import org.example.bankproject.user.jpa.Person;
 import org.example.bankproject.user.jpa.PersonRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class PersonService {
@@ -22,25 +24,24 @@ public class PersonService {
 
     public Person findByPersonId(Long personId) {
         return personRepository.findById(personId)
-                .orElseThrow(()-> new EntityNotFoundException("Person with id: " + personId + " not found"));
+                .orElseThrow(() -> new EntityNotFoundException("Person with id: " + personId + " not found"));
     }
 
     public PersonDto updatePersonalData(Long personId, PersonDto personDto) {
         Person person = findByPersonId(personId);
-        if (personDto.getEmail() != null) {
-            person.setEmail(personDto.getEmail());
-        }
-        if (personDto.getIdentityCardDto() != null) {
-            person.setIdentityCard(IdentityCardMapper.fromDto(personDto.getIdentityCardDto()));
-        }
-        if (personDto.getPhoneNumber() != null) {
-            person.setPhoneNumber(personDto.getPhoneNumber());
-        }
-        if (personDto.getAddressDto() != null) {
-            person.setAddress(AddressMapper.fromDto(personDto.getAddressDto()));
-        }
 
-        return PersonMapper.toDto(personRepository.save(person));
+        Person updatedPerson = person.toBuilder()
+                .email(Optional.ofNullable(personDto.getEmail()).orElse(person.getEmail()))
+                .phoneNumber(Optional.ofNullable(personDto.getPhoneNumber()).orElse(person.getPhoneNumber()))
+                .identityCard(Optional.ofNullable(personDto.getIdentityCardDto())
+                        .map(IdentityCardMapper::fromDto)
+                        .orElse(person.getIdentityCard()))
+                .address(Optional.ofNullable(personDto.getAddressDto())
+                        .map(AddressMapper::fromDto)
+                        .orElse(person.getAddress()))
+                .build();
+
+        return PersonMapper.toDto(personRepository.save(updatedPerson));
     }
 
     public PersonDto getPersonalData(Long personId) {
